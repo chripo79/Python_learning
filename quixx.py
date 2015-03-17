dba2089a749a39765069b32d707e83f18ecc6af6
chripo79 <ch.pohl@gmail.com>
2015-03-17 20:47:03 +0100

    - add to do list - add missing whitespaces

--- quixx.py
+++ quixx.py
@@ -12,26 +12,32 @@ template_row = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 template_miss = ['1', '2', '3', '4']
 dice = {"blue dice": 0, "red dice": 0, "yellow dice":  0, "green dice": 0, "1st white dice": 0,"2nd white dice": 0}
 
-colors = {1:"   Red: ",2:"Yellow: ",3:" Green: ",4:"  Blue: "}
+colors = {1:"   Red: ", 2: "Yellow: ", 3: " Green: ", 4: "  Blue: "}
+
+
 def draw_field():
     row = 1
-    while row <=4:
-        current =""
-        for c in range(0,len(template_row)):
-            if row<3:
-                current=current+" "+template_row[c]
+    while row <= 4:
+        current = ""
+        for c in range(0, len(template_row)):
+            if row <3 :
+                current=current + " " + template_row[c]
             else:
-                current=template_row[c]+" "+current
+                current=template_row[c] + " " + current
         print " %s%s " %(colors[row],current)
-        row+=1
+        row += 1
+
+
 def dice_roll():
-    roll=1
-    result =""
+    roll = 1
+    result = ""
 
     for k in dice:
         dice[k]=randint(1,6)
-        result=result+"["+str(roll)+"] "+k+": "+str(dice[k]) +" "
+        result=result + "[" + str(roll) + "] " + k + ": " + str(dice[k]) + " "
     print result
+
+
 
 draw_field()
 
